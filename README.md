2083e01617748e370dec3162f7f212d2c0099364
Tyson Bryant <bryantty@oregonstate.edu>
2023-11-29 18:46:02 -0800

    replaced with example code

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pokemon-Database
-Pokemon database portfolio project as part of CS340 Intro to Databases course.
--- app.js
+++ app.js
@@ -1,55 +1,250 @@
-const express = require('express');
-const app = express();
-const dbPool = require('./db-connector');
+// App.js
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // Serve static files from 'public' directory
+/*
+    SETUP
+*/
 
-// Cards CRUD Operations
-// Read all cards
-app.get('/cards', function(req, res) {
-    dbPool.query('SELECT * FROM Cards', function(error, results) {
-        if (error) res.status(500).send('Server error');
-        else res.json(results);
-    });
+// Express
+var express = require('express');
+var app = express();
+//app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));
+
+PORT = 1313;
+
+// Database
+var db = require('./database/db-connector');
+
+// Handlebars
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');
+//const { query } = require('express');
+app.engine('.hbs', engine({extname: ".hbs"}));
+app.set('view engine', '.hbs');
+
+
+/*
+    ROUTES
+*/
+
+// GET ROUTES
+app.get('/', function(req, res)
+{
+    // Declare Query 1
+    let query1;
+
+    // If there is no query string, we just perform a basic SELECT
+    if (req.query.lname === undefined)
+    {
+        query1 = "SELECT * FROM bsg_people;";
+    }
+
+    // If there is a query string, we assume this is a search, and return desired results
+    else
+    {
+        query1 = `SELECT * FROM bsg_people WHERE lname LIKE "${req.query.lname}%"`
+    }
+
+    // Query 2 is the same in both cases
+    let query2 = "SELECT * FROM bsg_planets;";
+
+    // Run the 1st query
+    db.pool.query(query1, function(error, rows, fields){
+        
+        // Save the people
+        let people = rows;
+        
+        // Run the second query
+        db.pool.query(query2, (error, rows, fields) => {
+            
+            // Save the planets
+            let planets = rows;
+
+            // Construct an object for reference in the table
+            // Array.map is awesome for doing something with each
+            // element of an array.
+            let planetmap = {}
+            planets.map(planet => {
+                let id = parseInt(planet.id, 10);
+
+                planetmap[id] = planet["name"];
+            })
+
+            // Overwrite the homeworld ID with the name of the planet in the people object
+            people = people.map(person => {
+                return Object.assign(person, {homeworld: planetmap[person.homeworld]})
+            })
+
+            return res.render('index', {data: people, planets: planets});
+        })
+    })
 });
 
-// Add a new card
-app.post('/cards', function(req, res) {
-    var { card_name, set_name, pokedex_number, type, image_path, rarity } = req.body;
-    var query = 'INSERT INTO Cards (card_name, set_name, pokedex_number, type, image_path, rarity) VALUES (?, ?, ?, ?, ?, ?)';
-    dbPool.query(query, [card_name, set_name, pokedex_number, type, image_path, rarity], function(error, results) {
-        if (error) res.status(500).send('Server error');
-        else res.status(201).send('Card added successfully');
-    });
+// POST ROUTES
+app.post('/add-person-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let homeworld = parseInt(data['input-homeworld']);
+    if (isNaN(homeworld))
+    {
+        homeworld = 'NULL'
+    }
+
+    let age = parseInt(data['input-age']);
+    if (isNaN(age))
+    {
+        age = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES ('${data['input-fname']}', '${data['input-lname']}', ${homeworld}, ${age})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/');
+        }
+    })
+})
+
+app.post('/add-person-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let homeworld = parseInt(data.homeworld);
+    if (isNaN(homeworld))
+    {
+        homeworld = 'NULL'
+    }
+
+    let age = parseInt(data.age);
+    if (isNaN(age))
+    {
+        age = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES ('${data.fname}', '${data.lname}', ${homeworld}, ${age})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT bsg_people.id, bsg_people.fname, bsg_people.lname, bsg_people.homeworld, bsg_people.age, bsg_planets.name 
+FROM bsg_people 
+LEFT JOIN bsg_planets ON bsg_people.homeworld = bsg_planets.id;`
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
 });
 
-// Update a card
-app.put('/cards/:card_id', function(req, res) {
-    var { card_name, set_name, pokedex_number, type, image_path, rarity } = req.body;
-    var card_id = req.params.card_id;
-    var query = 'UPDATE Cards SET card_name = ?, set_name = ?, pokedex_number = ?, type = ?, image_path = ?, rarity = ? WHERE card_id = ?';
-    dbPool.query(query, [card_name, set_name, pokedex_number, type, image_path, rarity, card_id], function(error, results) {
-        if (error) res.status(500).send('Server error');
-        else res.status(200).send('Card updated successfully');
-    });
-});
+app.delete('/delete-person-ajax/', function(req,res,next){
+    res.type('application/javascript');
+    let data = req.body;
+    let personID = parseInt(data.id);
+    let deleteBsg_Cert_People = `DELETE FROM bsg_cert_people WHERE pid = ?`;
+    let deleteBsg_People= `DELETE FROM bsg_people WHERE id = ?`;
+  
+  
+          // Run the 1st query
+          db.pool.query(deleteBsg_Cert_People, [personID], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
+              else
+              {
+                  // Run the second query
+                  db.pool.query(deleteBsg_People, [personID], function(error, rows, fields) {
+  
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.sendStatus(204);
+                      }
+                  })
+              }
+  })});
 
-// Delete a card
-app.delete('/cards/:card_id', function(req, res) {
-    var card_id = req.params.card_id;
-    var query = 'DELETE FROM Cards WHERE card_id = ?';
-    dbPool.query(query, [card_id], function(error, results) {
-        if (error) res.status(500).send('Server error');
-        else res.status(200).send('Card deleted successfully');
-    });
-});
+  app.put('/put-person-ajax', function(req,res,next){
+    let data = req.body;
+  
+    let homeworld = parseInt(data.homeworld);
+    let person = parseInt(data.fullname);
+  
+    let queryUpdateWorld = `UPDATE bsg_people SET homeworld = ? WHERE bsg_people.id = ?`;
+    let selectWorld = `SELECT * FROM bsg_planets WHERE id = ?`
+  
+          // Run the 1st query
+          db.pool.query(queryUpdateWorld, [homeworld, person], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
+              // If there was no error, we run our second query and return that data so we can use it to update the people's
+              // table on the front-end
+              else
+              {
+                  // Run the second query
+                  db.pool.query(selectWorld, [homeworld], function(error, rows, fields) {
+  
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.send(rows);
+                      }
+                  })
+              }
+  })});
 
-// TODO: Repeat similar CRUD operations for Sets, Pokemon, Decks, and Players
-
-// Start the server
-var PORT = 9000; // Replace with your chosen port number
-app.listen(PORT, function() {
-    console.log('Server running on http://flip1.engr.oregonstate.edu:' + PORT); // Replace 'flipX' with your actual FLIP server
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
