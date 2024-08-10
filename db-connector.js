bb4f46c222f84c3ff20a057aa951aef7615da318
Tyson Bryant <bryantty@oregonstate.edu>
2024-08-10 16:41:08 -0700

    Update db-connector.js
    Credentials outdated. Also contained sensitive info that didn't need to be here.

--- db-connector.js
+++ db-connector.js
@@ -1,14 +1,14 @@
 // Get an instance of mysql we can use in the app
 let mysql = require('mysql')
 
-// Create a 'connection pool' using the provided credentials
-let pool = mysql.createPool({
+// Create a 'connection pool' using the provided credentials. NOTE: Access to this db ended with class completion. Seeking new hosting method.
+/*let pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_armstlia',
-    password        : '5213',
-    database        : 'cs340_armstlia'
-})
+    user            : '',
+    password        : '',
+    database        : ''
+})*/
 
 // Export it for use in our application
 module.exports.pool = pool;
